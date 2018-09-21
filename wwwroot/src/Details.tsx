fd51f6df54e45b4da7baefe12e128373b04794a3
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-09-21 03:56:45 +0100

    Adding working redux bits

--- wwwroot/src/Details.tsx
+++ wwwroot/src/Details.tsx
@@ -1,25 +1,47 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-//import '../node_modules / bootstrap / dist / css / bootstrap.min.css';
-import {
-    Well
-} from "react-bootstrap";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { RootState } from './redux/index';
+import { incrementCounter } from './redux/details';
 
+import { Well } from 'react-bootstrap';
 
-export class Details extends React.Component<undefined, undefined> {
-
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Well className="outer-well">
-                <div>
-				this is the Details page
-				</div>
-            </Well>
-        )
-    }
+interface Props {
+  counter: number;
 }
 
+interface Actions {
+  incrementCounter: any;
+}
+
+interface TOwnProps extends RouteComponentProps<{ id: string }> {}
+
+interface State {}
+
+class DetailsInner extends React.Component<Props & Actions, State> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Well className="outer-well">
+        <div>this is the Details page</div>
+        <div>Counter : {this.props.counter}</div>
+        <div>
+          <button onClick={this.props.incrementCounter}>Increment</button>
+        </div>
+      </Well>
+    );
+  }
+}
+
+export const Details = connect<Props, Actions, TOwnProps, RootState>(
+  state => ({
+    counter: state.details.counter,
+  }),
+  {
+    incrementCounter: incrementCounter,
+  }
+)(DetailsInner);
--- wwwroot/src/Index.tsx
+++ wwwroot/src/Index.tsx
@@ -1,75 +1,66 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-
-import {
-    Nav,
-    Navbar,
-    NavItem,
-    NavDropdown,
-    MenuItem,
-    Button
-} from "react-bootstrap";
+import { store } from './redux/store';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
 import { Router, Route, hashHistory } from 'react-router';
-import { Details } from "./Details";
-import { Orders } from "./Orders";
-import "../scss/index.scss";
-
+import { Details } from './Details';
+import { Orders } from './Orders';
+import '../scss/index.scss';
 
 class MainNav extends React.Component<undefined, undefined> {
+  constructor(props: any) {
+    super(props);
+  }
 
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        return (
-			<Navbar collapseOnSelect>
-				<Navbar.Header>
-					<Navbar.Brand>
-						<span>Simple React-Redux example</span>
-					</Navbar.Brand>
-					<Navbar.Toggle />
-				</Navbar.Header>
-				<Navbar.Collapse>
-					<Nav pullRight>
-                        <NavItem eventKey={2} href='#/detailsPage'>Details</NavItem>
-                        <NavItem eventKey={2} href='#/ordersPage'>Orders</NavItem>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar> 
-        )
-    }
+  render() {
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <span>Simple React-Redux example</span>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={2} href="#/detailsPage">
+              Details
+            </NavItem>
+            <NavItem eventKey={2} href="#/ordersPage">
+              Orders
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 class App extends React.Component<undefined, undefined> {
-    render() {
-        return (
-            <div>
-                <div>
-                    <MainNav/>
-                    {this.props.children}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <MainNav />
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render((
+ReactDOM.render(
+  <Provider store={store}>
     <Router history={hashHistory}>
-        <Route component={App}>
-            <Route
-                path="/"
-                component={Details} />
-            <Route
-                path="/detailsPage"
-                component={Details} />
-            <Route
-                path="/ordersPage"
-                component={Orders} />
-        </Route>
+      <Route component={App}>
+        <Route path="/" component={Details} />
+		<Route path="/detailsPage" component={Details} />
+        <Route path="/ordersPage" component={Orders} />
+      </Route>
     </Router>
-), document.getElementById('root'));
-
+  </Provider>,
+  document.getElementById('root')
+);
